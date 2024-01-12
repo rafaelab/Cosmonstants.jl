2a7ef4efdc3076b933c7670a09f260b5f0b5de12
Rafael <rafaelab@users.noreply.github.com>
2024-01-12 10:05:03 +0100

    move CODATA definitions to main module

--- src/constants.jl
+++ src/constants.jl
@@ -1,5 +1,7 @@
 module Constants
 
+using PhysicalConstants.CODATA2018
+
 import Corpuscles: Particle
 import Reexport: @reexport
 import PhysicalConstants: @constant, @derived_constant
@@ -7,26 +9,8 @@ import PhysicalConstants: PhysicalConstant
 import PhysicalConstants.CODATA2018: c_0, ħ, h, k_B
 import Unitful: AbstractQuantity, @u_str, ustrip
 
-using PhysicalConstants
-
-for constant in names(PhysicalConstants.CODATA2018)
-	@eval @reexport import PhysicalConstants.CODATA2018: $constant
-end
-
 import PhysicalConstantsExtended: @createConstantsForMasses
 import PhysicalConstantsExtended: correspondingSymbol
-
-
-# import list of constants of `PhysicalConstants.jl` and reexport
-listOfConstantsBase = names(PhysicalConstants.CODATA2018)
-deleteat!(listOfConstantsBase, findall(x -> x == :CODATA2018, listOfConstantsBase))
-
-# bring all symbols corresponding to constants to the `Constants` scope
-# this ensures that these symbols can be easily imported directly from `PhysicalConstantsExtended` afterwards
-for constant in listOfConstantsBase
-	v = String(correspondingSymbol(constant))
-	@eval import PhysicalConstants.CODATA2018.($(Symbol(v)))
-end
 
 
 # constants commonly used in high-energy physics
@@ -37,5 +21,7 @@ include("astro.jl")
 
 # further  constants
 include("extended.jl")
+
+
 
 end
