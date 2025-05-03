5c266676e6b3855fc88a2c0b1167ce4fe87d4547
Rafael <rafaelab@users.noreply.github.com>
2025-05-03 15:41:45 +0200

    add some useful constants

--- src/constants/extended.jl
+++ src/constants/extended.jl
@@ -8,7 +8,17 @@ export DiracConstant
 
 # ----------------------------------------------------------------------------------------------- #
 #
-# const ħc = 
+@derived_constant(PlanckConstantTimesSpeedOfLight,
+	ħc, 
+	"Planck constant times speed of light",
+	convert(Float64, ustrip(big(ħ) * big(c_0))),
+	ustrip(big(ħ) * big(c_0)), 
+	u"J * m",
+	measurement(ħ) * measurement(c_0),
+	measurement(BigFloat, ħ) * measurement(BigFloat, c_0),
+	"definition"
+	)
+
 
 
 # ----------------------------------------------------------------------------------------------- #
