844af720c9fbf5cd05a12df38a7bc714afca89bc
Rafael <rafaelab@users.noreply.github.com>
2026-03-05 16:28:52 +0100

    rename tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,20 @@
 using Test
 using Cosmonstants
+using Corpuscles
 
 import Cosmonstants: 
 	UnitfullSystem,
 	UnitlessSystem,
 	Unitfull,
-	Unitless
+	Unitless,
+	getCorpusclesParticleMassUncertainty, 
+	getCorpusclesParticleMassValue
+using Measurements: 
+	measurement, 
+	uncertainty
 
 
-include("testPhysicalConstants.jl")
-include("testCorpuscles.jl")
-include("testToggler.jl")
+include("physicalConstants.jl")
+include("corpuscles.jl")
+include("toggler.jl")
+include("registry.jl")
